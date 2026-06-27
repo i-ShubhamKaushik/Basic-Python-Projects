d51f30200bdf8b97b456f710f2f3c60b54ce2b17
Shubham Kaushik <205813693+i-ShubhamKaushik@users.noreply.github.com>
2026-06-27 22:21:08 +0530

    Initial commit

---
+++ Project 10 - Quiz Application/project_10.py
@@ -0,0 +1,51 @@
+# PROJECT 10 - QUIZ APPLICATION
+
+import random
+
+questions = [
+    {
+        "question" : "Which keyword is used to define a function?",
+        "options" : {
+            "a" : "func",
+            "b" : "define",
+            "c" : "def",
+            "d" : "function",
+        },
+        "answer" : "c"
+    },
+
+    {
+        "question" : "Which data type is mutable?",
+        "options" : {
+            "a" : "tuple",
+            "b" : "list",
+            "c" : "string",
+            "d" : "int",
+        },
+        "answer" : "b"
+    },
+
+    {
+        "question" : "How we display an empty set?",
+        "options" : {
+            "a" : "{}",
+            "b" : "set{}",
+            "c" : "set()",
+            "d" : "[]",
+        },
+        "answer" : "c"
+    },
+
+    {
+        "question" : "Which anime has more than thousand episodes?",
+        "options" : {
+            "a" : "Naruto",
+            "b" : "One Piece",
+            "c" : "Bleach",
+            "d" : "Demon Slayer"
+        },
+        "answer" : "b"
+    },
+
+]
+
