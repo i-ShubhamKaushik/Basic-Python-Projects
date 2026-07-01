534ed49047de46bca4e56b8c668c544ed8638f01
Shubham Kaushik <205813693+i-ShubhamKaushik@users.noreply.github.com>
2026-07-01 21:31:36 +0530

    Student Management System

---
+++ Project 11 - Student Management System/project_11.py
@@ -0,0 +1,46 @@
+# PROJECT 11 - STUDENT MANAGEMENT SYSTEM
+
+students = {
+    "101": {
+        "name": "Shubham",
+        "age": 18,
+        "course": "B.Tech CSE",
+        "semester": 1,
+        "cgpa": 8.5
+    },
+
+    "102": {
+        "name": "Rahul",
+        "age": 19,
+        "course": "B.Tech CSE",
+        "semester": 2,
+        "cgpa": 8.1
+    }
+}
+
+while True:
+    user_choice= input(
+        "\n-- STUDENT MANAGEMENT SYSTEM --"
+        "\n1. Add Student"
+        "\n2. Search Student"
+        "\n3. Update Student"
+        "\n4. Delete Student"
+        "\n5. Show All Students"
+        "\n6. Exit"
+        "\n\n:"
+    )
+
+    if user_choice=="1":
+        pass
+    elif user_choice=="2":
+        pass
+    elif user_choice=="3":
+        pass
+    elif user_choice=="4":
+        pass
+    elif user_choice=="5":
+        pass
+    elif user_choice=="6":
+        break
+    else:
+        print("\nInvalid Arguement...!\n")
